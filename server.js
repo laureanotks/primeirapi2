63a6da89950b9b16eb2cabc8ada35e827e283add
Pedro Henrique Laureano Schuwartz <pedrolaureano598@gmail.com>
2023-08-16 12:13:28 -0300

    fix

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('Running at port 3000'))
+app.listen(3000, () => console.log('Running at port 3000!'))
